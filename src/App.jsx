e8722170c9cf7843ff315f457b15f0296459188d
Mehdi Dib <dib.mehdi13@gmail.com>
2025-03-02 23:48:38 +0100

    Setting up shadcn and tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      
+      <p className="text-lg text-gray-500">Project initialisation</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
