3de9f8fc085c8f46565a9f142746346e0e7013b9
Mehdi Dib <dib.mehdi13@gmail.com>
2025-03-13 17:00:41 +0100

    refactor:apiStudentSkills

--- src/modules/student/api/apiStudentSkills.js
+++ src/modules/student/api/apiStudentSkills.js
@@ -1,5 +1,15 @@
 import { axiosPrivate } from "@/api/axios";
 
+export async function getAvailableSkills() {
+  try {
+    const response = await axiosPrivate.get("/students/skills");
+    return response.data.data;
+  } catch (error) {
+    console.error("error while getting student skills:", error);
+    throw error;
+  }
+}
+
 export async function addStudentSkills(skillsWithStudentId) {
   try {
     const response = await axiosPrivate.post(
--- src/modules/student/features/useAddStudentSkills.js
+++ src/modules/student/features/useAddStudentSkills.js
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { addStudentSkills } from "../api/addStudentSkills";
+
 import toast from "react-hot-toast";
+import { addStudentSkills } from "../api/apiStudentSkills";
 
 export function useAddStudentSkills() {
   const queryClient = useQueryClient();
